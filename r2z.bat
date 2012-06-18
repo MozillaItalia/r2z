4354ead12175ce50c31bf8034de0e5f737d20ccc
Sandro Della Giustina <gialloporpora@yahoo.it>
2012-06-18 14:49:02 +0200

    Modificato r2z.bat per copiare i rilasci nella cartella su Dropbox. Bisogna impostare nel file il giusto path della cartella sul proprio computer

--- r2z.bat
+++ r2z.bat
@@ -1,4 +1,6 @@
 @echo off
+set releasedbfolder=c:\dropbox\release2zip\latest
+set olddbfolder=c:\dropbox\release2zip\old
 set author=%1
 if "%1"=="" set author=gialloporpora
 set name=Release2Zip-%date:~6,4%%date:~3,2%%date:~0,2%@%author%.7z
@@ -6,9 +8,7 @@ set name=Release2Zip-%date:~6,4%%date:~3,2%%date:~0,2%@%author%.7z
 7z a %name%  @files.txt
 if %errorlevel% neq 0  goto end
 md5sum  %name%
-
-move %name% download
-a
+move %releasedbfolder%\* %olddbfolder%
+move %name% %releasedbfolder%
 echo @@@ %name% released @@@@
-
 :end:
